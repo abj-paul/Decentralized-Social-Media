ee31edcf17f12d8bff779c3ca082f59307dfc0ad
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-07-21 19:13:00 +0600

    added feature : Get images from minio

--- backend/minio.js
+++ backend/minio.js
@@ -5,6 +5,8 @@ const Minio = require('minio');
 const app = express();
 const port = 3000;
 
+const uploadedImages = []; // Array to store image name and object name associations
+
 // Set up MinIO client
 const minioClient = new Minio.Client({
   endPoint: 'localhost',
@@ -35,6 +37,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
 
   const bucketName = 'posts'; // Replace with your desired bucket name
   const objectName = req.file.originalname;
+  const imageName = req.file.originalname; // Save the image name
 
   minioClient.fPutObject(bucketName, objectName, filePath, metaData, (err, etag) => {
     if (err) {
@@ -43,8 +46,39 @@ app.post('/upload', upload.single('image'), (req, res) => {
     }
 
     console.log('Image uploaded successfully: ' + objectName);
+
+    // Save the image name and object name association in the array
+    uploadedImages.push({ imageName, objectName });
+
+    console.log(uploadedImages);
     return res.status(200).send('Image uploaded successfully.');
   });
+});
+
+// Endpoint to get the image by name
+app.get('/images/:imageName', (req, res) => {
+  const { imageName } = req.params;
+
+  // Find the corresponding object name in the array
+  const uploadedImage = uploadedImages.find(img => img.imageName === imageName);
+
+  if (!uploadedImage) {
+    return res.status(404).send('Image not found.');
+  }
+
+  const bucketName = 'posts'; // Replace with your desired bucket name
+  const objectName = uploadedImage.objectName;
+
+  minioClient.getObject(bucketName, objectName, (err, dataStream) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error retrieving the image.');
+    }
+
+    // Pipe the data stream to the response to serve the image
+    dataStream.pipe(res);
+  });
+
 });
 
 app.listen(port, () => {
